a7f3b11aa1eead3b3bf98d2390c3f1ff0fd1ecdc
Yi <yi@Yis-MacBook-Pro.local>
2015-04-22 21:27:38 -0400

    typo

--- src/filesys/filesys.c
+++ src/filesys/filesys.c
@@ -114,7 +114,7 @@ filesys_create (const char *name, off_t initial_size, bool isdir)
    otherwise.
    Fails if no file named NAME exists,
    or if an internal memory allocation fails. */
-struct void
+void
 filesys_open (const char *name, struct file **file, struct dir **dir, bool *isdir)
 {
   // struct dir *dir = dir_open_root ();
